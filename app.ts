4f7eabdeb1ee40e251ee2bc37f2e795e0c4bf374
Sean Wong <seanwong24@live.com>
2024-06-06 22:00:04 +0000

    minor fix

--- app.ts
+++ app.ts
@@ -3,5 +3,5 @@ import { startApp } from "https://raw.githubusercontent.com/gaia-web/exp_deno-mi
 startApp({
   OPENAPI_FILE_PATH: "./openapi.json",
   FRONTENDS:
-    `/,./www/main,./index.html,./index.html:/admin,./www/admin,./index.html,./index.html:/swagger,./www/swagger,./index.html,./index.html`,
+    `,./www/main,./index.html,./index.html:/admin,./www/admin,./index.html,./index.html:/swagger,./www/swagger,./index.html,./index.html`,
 });
