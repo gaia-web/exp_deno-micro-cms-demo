7f312e7bbecd25494d4d2121fc420344508881c8
Sean Wong <seanwong24@live.com>
2024-06-06 23:57:05 +0000

    update dmc import url

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { startApp } from "https://raw.githubusercontent.com/gaia-web/exp_deno-micro-cms/5b57ceff56af1dbfcd2af5ac03bd7290a42692c6/mod.ts";
+import { startApp } from "https://raw.githubusercontent.com/SeanWong24/exp_deno-micro-cms/5b57ceff56af1dbfcd2af5ac03bd7290a42692c6/mod.ts";
 
 startApp({
   OPENAPI_FILE_PATH: "./openapi.json",
