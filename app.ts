d8f6bd8bc1a4d12c32ceafef42582d176628059d
Sean Wong <seanwong24@live.com>
2025-08-24 00:14:32 +0000

    update deps

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
-import { startApp } from "https://raw.githubusercontent.com/SeanWong24/exp_deno-micro-cms/5b57ceff56af1dbfcd2af5ac03bd7290a42692c6/mod.ts";
+import { setupApp } from "https://raw.githubusercontent.com/SeanWong24/exp_deno-micro-cms/491350087cc0bb0eca78c6c2c0e301c49c23f699/mod.ts";
 
-startApp({
+const app = await setupApp({
   OPENAPI_FILE_PATH: "./openapi.json",
   FRONTENDS:
     `/admin,./www/admin,./index.html,./index.html:/swagger,./www/swagger,./index.html,./index.html:,./www/main,./index.html,./index.html`,
 });
+
+export default { fetch: app.fetch };
